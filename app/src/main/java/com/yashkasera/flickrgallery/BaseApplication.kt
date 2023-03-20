b66a34a6e440626334ffb137fc4f51a83ecbb08e
Yash Kasera <yashkasera@icloud.com>
2023-03-20 09:59:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/yashkasera/flickrgallery/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.yashkasera.flickrgallery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()
---
+++ app/src/main/java/com/yashkasera/flickrgallery/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.yashkasera.flickrgallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yashkasera.flickrgallery.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
